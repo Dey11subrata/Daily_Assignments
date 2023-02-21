1acad1d800aebf0a16dd93d7aca3b29fe219ef40
subrata <deysubrata2311@gmail.com>
2023-02-21 08:44:29 +0530

    code4 updated

--- Day_01/Code4.java
+++ Day_01/Code4.java
@@ -1,1 +1,17 @@
 // 4. Swap two numberswithout using third variable approach 1. 
+
+public class Code4{
+	public static void main(String[] args){
+		
+		int a = 10 ; 
+		int b = 20;
+		
+		System.out.println("before swap a:"+a+" b:"+b );
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("after swap a:"+a+" b:"+b );
+	}
+}
